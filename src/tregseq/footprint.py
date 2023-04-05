0ae7f4e80a0beaf41595d371d44cbff041258583
Rosalind Pan <rosalindpan@rpmacbook.local>
2023-04-04 23:06:59 -0700

    rewrote code for for calculating information footprint

--- src/tregseq/footprint.py
+++ src/tregseq/footprint.py
@@ -47,36 +47,65 @@ def get_p_b(all_mutarr, n_seqs):
     return np.asarray([1 - p_mut, p_mut]).T
 
 
-def get_p_mu(ex_data, nbins, upper_bound):
+def bin_expression_levels(mu_data, nbins, upper_bound):
+    bins = np.linspace(0, upper_bound, nbins, dtype=int).tolist()
+    bins.append(int(max(mu_data) + 1))
 
-    bins = np.linspace(0, upper_bound, nbins, dtype=int)
-    out = pd.cut(ex_data, bins=bins,
-                include_lowest=True, right=False)
-    cnt = out.value_counts().tolist()
-    cnt.append(len(ex_data) - sum(cnt))
-    p_mu = np.asarray(cnt) / sum(cnt)
-    return p_mu
-
-
-def mi_footprint(fpath_to_seqcnt, wtseq):
-
-    df = pd.read_csv(fpath_to_seqcnt)
-
-    all_mutarr = match_seqs(df['seq'].values, wtseq)
-    all_wtarr = utils.flip_boolean(all_mutarr)
-
-    seq_cnt = df[['ct_0', 'ct_1']].apply(np.sum).values
-    seq_cnt /= np.sum(df['ct'])
-
-    p_mut_tot = count_mut_tot(all_wtarr, all_mutarr, df['ct'].values)
-
-    p_wt_dna = count_mut(all_wtarr, df['ct_0'].values, df['ct'].values)
-    p_wt_rna = count_mut(all_wtarr, df['ct_1'].values, df['ct'].values)
-    p_mut_dna = count_mut(all_mutarr, df['ct_0'].values, df['ct'].values)
-    p_mut_rna = count_mut(all_mutarr, df['ct_1'].values, df['ct'].values)
-
-    info_fp = MI(p_mut_tot, seq_cnt,
-                p_wt_dna, p_wt_rna, p_mut_dna, p_mut_rna,
-                seqlen=len(wtseq))
+    binned = pd.cut(mu_data, bins=bins,
+                                labels=np.arange(nbins),
+                                include_lowest=True, right=False)
     
-    return info_fp
+    mu_bins = binned.values
+    bin_cnt = binned.value_counts().values
+    return mu_bins, bin_cnt
+
+
+def get_p_mu(bin_cnt, n_seqs):
+    return bin_cnt / n_seqs
+
+
+def get_joint_p(all_mutarr, mu_bins, nbins, n_seqs,
+                pseudocount=10**(-6), len_promoter=160):
+    list_joint_p = []
+    for position in range(len_promoter):
+        joint_p = np.zeros((2, nbins)) + pseudocount
+        # adding a pseudocount to prevent zero-division error
+        for i in range(n_seqs):
+            for j in range(nbins):
+                if (all_mutarr[i][position] == 0) & (mu_bins[i] == j):
+                    joint_p[0][j] += 1
+                elif (all_mutarr[i][position] == 1) & (mu_bins[i] == j):
+                    joint_p[1][j] += 1
+
+        joint_p /= np.sum(joint_p)
+        list_joint_p.append(joint_p)
+    return list_joint_p
+
+
+def MI(list_p_b, p_mu, list_joint_p):
+    mutual_info = []
+    for position in range(160):
+        p_b = list_p_b[position]
+        joint_p = list_joint_p[position]
+
+        mi = 0
+        for i in range(len(p_mu)):
+            mi += joint_p[0][i] * np.log2(joint_p[0][i] / (p_b[0] * p_mu[i]))
+            mi += joint_p[1][i] * np.log2(joint_p[1][i] / (p_b[1] * p_mu[i]))
+        mutual_info.append(mi)
+    return mutual_info
+
+
+def get_info_footprint(mut_list, mu_data, wtseq,
+                       nbins, upper_bound,
+                       pseudocount=10**(-6), len_promoter=160):
+    n_seqs = len(mut_list)
+
+    all_mutarr = match_seqs(mut_list, wtseq)
+    list_p_b = get_p_b(all_mutarr, n_seqs)
+    mu_bins, bin_cnt = bin_expression_levels(mu_data, nbins, upper_bound)
+    p_mu = get_p_mu(bin_cnt, n_seqs)
+    list_joint_p = get_joint_p(all_mutarr, mu_bins, nbins, n_seqs,
+                               pseudocount=pseudocount, len_promoter=len_promoter)
+    footprint = MI(list_p_b, p_mu, list_joint_p)
+    return footprint
