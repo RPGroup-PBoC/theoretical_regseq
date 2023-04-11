15c791b4ffd2a676326a049934bce6af4f808d60
Rosalind Pan <rosalindpan@rpmacbook.local>
2023-04-11 13:44:20 -0700

    compared old and new footprints; exploring optimal bin sizes

--- src/tregseq/footprint.py
+++ src/tregseq/footprint.py
@@ -42,6 +42,8 @@ def get_p_b(all_mutarr, n_seqs):
             at each base position.
     '''    
 
+    #tot_mut_cnt = np.sum(all_mutarr * mu_data.values[:, np.newaxis], axis=0)
+    #p_mut = tot_mut_cnt / np.sum(mu_data)
     tot_mut_cnt = np.sum(all_mutarr, axis=0)
     p_mut = tot_mut_cnt / n_seqs
 
@@ -53,8 +55,8 @@ def bin_expression_levels(mu_data, nbins, upper_bound):
     bins.append(int(max(mu_data) + 1))
 
     binned = pd.cut(mu_data, bins=bins,
-                                labels=np.arange(nbins),
-                                include_lowest=True, right=False)
+                    labels=np.arange(nbins),
+                    include_lowest=True, right=False)
     
     mu_bins = binned.values
     bin_cnt = binned.value_counts().values
@@ -131,3 +133,71 @@ def get_expression_shift(mut_list, mu_data, wtseq,
         exshift_list = smoothing(exshift_list, windowsize=windowsize)
     
     return exshift_list
+
+
+## code to calculate information footprint based on the eLife paper
+
+def flip_boolean(arr):
+
+    nrow, ncol = arr.shape
+    flipped_arr = np.zeros((nrow, ncol))
+    for i in range(nrow):
+        for j in range(ncol):
+            if arr[i][j] == 0:
+                flipped_arr[i][j] = 1
+
+    return flipped_arr
+
+
+def count_mut(all_mutarr, cnt_seq, tot_cnt_seq):
+
+    _cnt = np.multiply(all_mutarr, cnt_seq[:, np.newaxis])
+    cnt = np.sum(_cnt, axis=0) / np.sum(tot_cnt_seq)
+    return cnt
+
+
+def count_tot(all_wtarr, all_mutarr, cnt_seq):
+    wt_cnt = np.multiply(all_wtarr, np.asarray(cnt_seq)[:, np.newaxis])
+    mut_cnt = np.multiply(all_mutarr, np.asarray(cnt_seq)[:, np.newaxis])
+    tot_wt_cnt = np.sum(wt_cnt, axis=0)
+    tot_mut_cnt = np.sum(mut_cnt, axis=0)
+    _cnt = np.stack([tot_wt_cnt, tot_mut_cnt])
+    cnt = _cnt / np.sum(_cnt, axis=0)
+    return cnt
+
+
+def MI_old(p_mut_tot, seq_cnt,
+       p_wt_dna, p_wt_rna, p_mut_dna, p_mut_rna,
+       seqlen=160):
+
+    mutual_info = []
+    for i in range(seqlen):
+        mi = p_wt_dna[i] * np.log2(p_wt_dna[i] / (p_mut_tot[:, i][0] * seq_cnt[0]))
+        mi += p_wt_rna[i] * np.log2(p_wt_rna[i] / (p_mut_tot[:, i][0] * seq_cnt[1]))
+        mi += p_mut_dna[i] * np.log2(p_mut_dna[i] / (p_mut_tot[:, i][1] * seq_cnt[0]))
+        mi += p_mut_rna[i] * np.log2(p_mut_rna[i] / (p_mut_tot[:, i][1] * seq_cnt[1]))
+        mutual_info.append(mi)
+        
+    return mutual_info
+
+
+def footprint_old(df, wtseq):
+
+    all_mutarr = match_seqs(df['seq'].values, wtseq)
+    all_wtarr = flip_boolean(all_mutarr)
+
+    seq_cnt = df[['ct_0', 'ct_1']].apply(np.sum).values
+    seq_cnt /= np.sum(df['ct'])
+
+    p_mut_tot = count_tot(all_wtarr, all_mutarr, df['ct'].values)
+
+    p_wt_dna = count_mut(all_wtarr, df['ct_0'].values, df['ct'].values)
+    p_wt_rna = count_mut(all_wtarr, df['ct_1'].values, df['ct'].values)
+    p_mut_dna = count_mut(all_mutarr, df['ct_0'].values, df['ct'].values)
+    p_mut_rna = count_mut(all_mutarr, df['ct_1'].values, df['ct'].values)
+
+    info_fp = MI_old(p_mut_tot, seq_cnt,
+                p_wt_dna, p_wt_rna, p_mut_dna, p_mut_rna,
+                seqlen=len(wtseq))
+    
+    return info_fp
